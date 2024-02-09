34f52e1bda099f6b1c7a0c71d0838396587b15ee
MichaelCavell <96754228+MichaelCavell@users.noreply.github.com>
2024-02-09 12:50:49 -0500

    Complete TOP Mini Message Board project

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Messages', messages: messages });
 });
+
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'New Message' });
+});
+
+router.post('/new', function(req, res, next) {
+  const { user, text } = req.body;
+  
+  const newMessage = {
+    user: user,
+    text: text,
+    added: new Date()
+  };
+
+  messages.push(newMessage);
+
+  res.redirect('/');
+});
+
 
 module.exports = router;
