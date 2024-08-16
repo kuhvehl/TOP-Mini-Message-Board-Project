3e8bc1b9829c7716720fc5f3d34458109979965a
kuhvehl <96754228+kuhvehl@users.noreply.github.com>
2024-08-16 15:55:52 -0400

    Update Port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const indexRouter = require("./routes/index");
 const newRouter = require("./routes/new");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
