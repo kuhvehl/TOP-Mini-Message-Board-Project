f8497ed7b5ce07b1e74f917f1d52405a3b0b6bdc
kuhvehl <96754228+kuhvehl@users.noreply.github.com>
2024-08-16 11:41:38 -0400

    Setup files and routes for index and new message

--- app.js
+++ app.js
@@ -7,9 +7,14 @@ const PORT = 3000;
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.render("index", { message: "Mini Message Board!" });
+});
+
+app.get("/new", (req, res) => {
+  res.render("new");
 });
 
 app.listen(PORT, () => {
