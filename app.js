1f26ad54b9eb225a4e53fd35ce03975f070b9b8d
kuhvehl <96754228+kuhvehl@users.noreply.github.com>
2024-08-16 12:24:25 -0400

    Set up index to loop through and display messages

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 const express = require("express");
 const path = require("path");
 
+const indexRouter = require("./routes/index");
+const newRouter = require("./routes/new");
+
 const app = express();
 const PORT = 3000;
 
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
-
-app.get("/", (req, res) => {
-  res.render("index", { message: "Mini Message Board!" });
-});
-
-app.get("/new", (req, res) => {
-  res.render("new");
-});
+app.use("/", indexRouter);
+app.use("/new", newRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
