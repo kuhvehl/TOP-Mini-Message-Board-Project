5010da3ea1e8a2226604f5c3585d30a17179e062
kuhvehl <96754228+kuhvehl@users.noreply.github.com>
2024-08-16 14:10:10 -0400

    Add an open buttons for messages.
    Add an “open” button next to every message to open
    a new page with the message details.

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRouter);
 app.use("/new", newRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -14,8 +14,33 @@ const messages = [
   },
 ];
 
+const index = 0;
+
 router.get("/", (req, res) => {
   res.render("index", { title: "Mini Message Board", messages: messages });
+});
+
+router.get("/message/:id", (req, res) => {
+  const messageId = parseInt(req.params.id, 10);
+  const message = messages.find((msg, index) => index === messageId);
+
+  if (message) {
+    res.render("message", { title: "Message Details", message: message });
+  } else {
+    res.status(404).send("Message not found");
+  }
+});
+
+router.post("/new", (req, res) => {
+  const newMessage = {
+    user: req.body.user,
+    text: req.body.message,
+    added: new Date(),
+  };
+
+  messages.push(newMessage);
+
+  res.redirect("/");
 });
 
 module.exports = router;
